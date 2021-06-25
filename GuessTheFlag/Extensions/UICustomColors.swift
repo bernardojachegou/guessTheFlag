c19370d1e42072febc8ac30586c4262e1d4963c1
Michel Bernardo <bernardoenglish@outlook.com>
2021-06-24 23:14:31 -0300

    new colors set created

--- GuessTheFlag/Extensions/UICustomColors.swift
+++ GuessTheFlag/Extensions/UICustomColors.swift
@@ -9,21 +9,12 @@ import UIKit
 
 extension UIColor {
     static var primaryColor: UIColor {
-        return UIColor(red: 42/255, green: 42/255, blue: 87/255, alpha: 1)
+        return UIColor(named: "PrimaryColor")!
     }
     static var secondaryColor: UIColor {
-        return UIColor(red: 244/255, green: 212/255, blue: 4/255, alpha: 1)
+        return UIColor(named: "SecondaryColor")!
     }
-    static var bgColor: UIColor {
-        return UIColor(red: 60/255, green: 60/255, blue: 76/255, alpha: 1)
-    }
-    static var sdwColor: UIColor {
-        return UIColor(red: 11/255, green: 9/255, blue: 4/255, alpha: 1)
-    }
-    static var wrongAnswerColor: UIColor {
-        return UIColor(red: 179/255, green: 0/255, blue: 33/255, alpha: 1)
-    }
-    static var correctAnswerColor: UIColor {
-        return UIColor(red: 20/255, green: 128/255, blue: 1/255, alpha: 1)
+    static var shadowColor: UIColor {
+        return UIColor(named: "ShadowColor")!
     }
 }
